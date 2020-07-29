f896cc8cd87ecf9817723900a2dce6d2d9e3e85c
kyle-quinlan <kquinlan292@gmail.com>
2020-07-29 13:34:45 -0400

    Implemented Selection Sort Function
    Returns an array of sorted integers back to the main file.

--- src/Algorithms/selectionSort.js
+++ src/Algorithms/selectionSort.js
@@ -1,3 +1,18 @@
 export const selectionSort = (array) => {
-  return "foo";
+  const arrayLength = array.length;
+  const arr = array;
+
+  for (var i = 0; i < arrayLength - 1; i++) {
+    var min_index = i;
+    for (var j = i + 1; j < arrayLength; j++) {
+      if (arr[j] < arr[min_index]) {
+        min_index = j;
+      }
+    }
+    var temp = arr[min_index];
+    arr[min_index] = arr[i];
+    arr[i] = temp;
+  }
+
+  return arr;
 };
